6c0a57f3d2ea7a36386025813e7427aa3c8fe902
tk <kexf7pqsdu@snkmail.com>
2017-11-04 16:18:33 -0500

    04 - first app

--- 04 first-app/Main.elm
+++ 04 first-app/Main.elm
@@ -4,33 +4,58 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
+import String
 
 -- model
 
 
 type alias Model =
-    Int
+    { total: Int
+    , delta: Int
+    , error: Maybe String
+    }
 
 
 initModel : Model
 initModel =
-    0
-
+    { total = 0
+    , delta = 0
+    , error = Nothing
+    }
 
 
 -- update
 
 
 type Msg
-    = AddCalorie
+    = UpdateDelta String
+    | AddCalories
     | Clear
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        AddCalorie ->
-            model + 1
+        UpdateDelta deltaStr ->
+            case String.toInt deltaStr of
+                Ok delta ->
+                    { model
+                        | delta = delta
+                        , error = Nothing
+                    }
+
+                Err err ->
+                    { model
+                        | delta = 0
+                        , error = Just err
+                    }
+
+        AddCalories ->
+            { model
+                | total = model.total + model.delta
+                , delta = 0
+                , error = Nothing
+            }
 
         Clear ->
             initModel
@@ -44,17 +69,33 @@ view : Model -> Html Msg
 view model =
     div []
         [ h3 []
-            [ text ("Total Calories: " ++ (toString model)) ]
-        , button
-            [ type_ "button"
-            , onClick AddCalorie
+            [ text ("Total Calories: " ++ (toString model.total)) ]
+        , input
+            [ type_ "text"
+            , placeholder "Number of calories to add ..."
+            , name "calorieCount"
+            , value
+                ( if model.delta == 0 then
+                    ""
+                else
+                    toString model.delta
+                )
+            , onInput UpdateDelta
             ]
-            [ text "Add" ]
-        , button
-            [ type_ "button"
-            , onClick Clear
+            []
+        , div [] [ text (Maybe.withDefault "" model.error) ]
+        , div []
+            [ button
+                [ type_ "button"
+                , onClick AddCalories
+                ]
+                [ text "Add" ]
+            , button
+                [ type_ "button"
+                , onClick Clear
+                ]
+                [ text "Clear" ]
             ]
-            [ text "Clear" ]
         ]
 
 
