a33b75192dbe17bd5e8c3bd510c05349ee6d21dc
tk <kexf7pqsdu@snkmail.com>
2017-11-04 16:17:49 -0500

    05 - scorekeeper-starter

---
+++ 05 scorekeeper-starter/Main.elm
@@ -0,0 +1,129 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+-- model
+
+
+type alias Model =
+    { players : List Player
+    , name : String
+    , playerId : Maybe Int
+    , plays : List Play
+    }
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    , points : Int
+    }
+
+
+type alias Play =
+    { id : Int
+    , playerId : Int
+    , name : String
+    , points : Int
+    }
+
+
+initModel : Model
+initModel =
+    { players = []
+    , name = ""
+    , playerId = Nothing
+    , plays = []
+    }
+
+
+
+-- update
+
+
+type Msg
+    = Edit Player
+    | Score Player Int
+    | Input String
+    | Save
+    | Cancel
+    | DeletePlay Play
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input name ->
+            { model | name = name }
+
+        Save ->
+            if (String.isEmpty model.name) then
+                model
+            else
+                { model
+                | players = model.players ++
+                    [Player
+                        (List.length model.players)
+                        model.name
+                        0
+                        ]
+                , name = "" }
+
+        Cancel ->
+            { model | name = "" }
+
+        _ ->
+            model
+
+
+save : Model -> Model
+save model =
+    if (model.playerId == Nothing) then
+        { model
+        | players = model.players ++
+            [Player
+                (List.length model.players)
+                model.name
+                0
+                ]
+        , name = "" }
+    else
+        model
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "scoreboard" ]
+        [ h1 [] [ text "Score Keeper" ]
+        , playerForm model
+        , p [] [ text (toString model) ]
+        ]
+
+
+playerForm : Model -> Html Msg
+playerForm model =
+    Html.form [ onSubmit Save ]
+        [ input
+            [ type_ "text"
+            , placeholder "Add/Edit Player..."
+            , onInput Input
+            , value model.name
+            ]
+            []
+        , button [ type_ "submit" ] [ text "Save" ]
+        , button [ type_ "button", onClick Cancel ] [ text "Cancel" ]
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
